a9a7ef89ec6334fe6e4407a21921165f4e4311e0
harry <gabbiharkaran@gmail.com>
2023-01-04 22:44:18 -0800

    repeat

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql2');
 
-var con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
+
+// var con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+});
+var con = mysql.createConnection({
+    host: "sql6.freesqldatabase.com",
+    user: "sql6588369",
+    password: "dtQ5iD5mXx",
+    database: "josql6588369ke"
+});
 con.connect(function(err) {
     if (err) throw err;
 });
